bfb76eaa0add0ae28517cb0149597c6588611f35
Shubhamdutta2000 <dondutta2000@gmail.com>
2022-02-27 04:58:03 +0530

    [new] added footer in layout

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react'
 // components
 import Navbar from 'components/Navbar'
+import Footer from 'components/Footer'
 
 export default function Layout({ children }) {
     return (
         <>
             <Navbar />
             {children}
+            <Footer />
         </>
     )
 }
