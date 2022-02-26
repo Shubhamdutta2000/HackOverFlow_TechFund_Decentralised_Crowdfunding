76ee96393b8a233987f62c398d747e478255773a
AmitrajitDas <amitrajitdas31@gmail.com>
2022-02-26 13:44:18 +0530

    update: form handling

--- components/CreateIdea/IdeaForm/index.jsx
+++ components/CreateIdea/IdeaForm/index.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { useState } from 'react'
 import {
   Grid,
   TextField,
@@ -10,31 +11,76 @@ import {
 import { useStyles } from '../../../styles/CreateIdea/ideaForm/ideaForm.style'
 
 const IdeaForm = () => {
+  const [formData, setFormData] = useState({
+    title: '',
+    description: '',
+    threshold: '',
+    fund: '',
+    idea: '',
+  })
   const classes = useStyles()
-
   const isMobile = useMediaQuery('(max-width:600px)')
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    setFormData((f) => ({
+      ...f,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log(formData)
+  }
 
   return (
     <div className={classes.container}>
       <Grid container spacing={'14'} component='main'>
         <Grid item xs={12} sm={12} md={6} component={Paper} elevation={0}>
-          <form className={classes.form}>
+          <form className={classes.form} onSubmit={submitHandler}>
             <label>Title of your idea</label>
-            <TextField variant='outlined' name='title' />
+            <TextField
+              variant='outlined'
+              name='title'
+              value={formData.title}
+              onChange={(e) => handleChange(e)}
+            />
             <label>Short description</label>
-            <TextField variant='outlined' />
+            <TextField
+              variant='outlined'
+              name='description'
+              value={formData.description}
+              onChange={(e) => handleChange(e)}
+            />
             <label>Minimum amount threshold</label>
-            <TextField variant='outlined' />
+            <TextField
+              variant='outlined'
+              name='threshold'
+              value={formData.threshold}
+              onChange={(e) => handleChange(e)}
+            />
             <label>Total fund required</label>
-            <TextField variant='outlined' />
+            <TextField
+              variant='outlined'
+              name='fund'
+              value={formData.fund}
+              onChange={(e) => handleChange(e)}
+            />
             <label>Your idea in detail</label>
             <TextareaAutosize
-              aria-label='minimum height'
               minRows={3}
-              placeholder='Minimum 3 rows'
               className={classes.textarea}
+              name='idea'
+              value={formData.idea}
+              onChange={(e) => handleChange(e)}
             />
-            <Button variant='contained' color='button' className={classes.btn}>
+            <Button
+              variant='contained'
+              color='button'
+              className={classes.btn}
+              type='submit'
+            >
               Submit Idea
             </Button>
           </form>
