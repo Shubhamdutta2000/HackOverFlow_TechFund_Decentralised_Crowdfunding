c0fabf247194e664c22548f523a4a9ff1c247540
AmitrajitDas <amitrajitdas31@gmail.com>
2022-02-26 13:53:19 +0530

    feat: Create Idea Page

--- config/theme.js
+++ config/theme.js
@@ -1,19 +1,40 @@
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles'
+import { red } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#556cd6',
-        },
-        secondary: {
-            main: '#19857b',
-        },
-        error: {
-            main: red.A400,
-        },
+  palette: {
+    primary: {
+      main: '#556cd6',
     },
-});
+    secondary: {
+      main: '#19857b',
+    },
+    button: {
+      main: '#333CFF',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
 
-export default theme;
+  typography: {
+    h2: {
+      fontFamily: 'Circular Std',
+      fontStyle: 'normal',
+      fontWeight: '500',
+      fontSize: '72px',
+      lineHeight: '107%',
+    },
+    body1: {
+      fontFamily: 'Euclid Circular A',
+      fontStyle: 'normal',
+      fontWeight: '500',
+      fontSize: '27px',
+      lineHeight: '107%',
+      color: '#252525',
+    },
+  },
+})
+
+export default theme
---
+++ pages/createIdea/index.js
@@ -0,0 +1,13 @@
+import IdeaHeader from '../../components/CreateIdea/IdeaHeader'
+import IdeaForm from '../../components/CreateIdea/IdeaForm'
+
+const CreateIdea = () => {
+  return (
+    <div>
+      <IdeaHeader />
+      <IdeaForm />
+    </div>
+  )
+}
+
+export default CreateIdea
