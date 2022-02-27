b28c9f431686277ee4eb4183edf4ecab93b5ad6a
Shubhamdutta2000 <dondutta2000@gmail.com>
2022-02-27 19:17:05 +0530

    [NEW] added related campaigns section in individual idea page

--- pages/ideas/[ideas].js
+++ pages/ideas/[ideas].js
@@ -1,9 +1,14 @@
+import { Container, Typography } from '@mui/material'
+import CampaignCarousal from 'components/CampaignsCarousal'
 import Layout from 'layout/Layout'
 import IdeaBody from '../../components/IdeaBody'
+
 const IndividualIdeaPage = () => {
+
   return (
     <Layout>
       <IdeaBody />
+      <CampaignCarousal heading="Related Campaigns" />
     </Layout>
   )
 }
