08ad35831892b1b43a90d8f33b35409f3a12124d
Shubhamdutta2000 <dondutta2000@gmail.com>
2022-02-27 20:36:18 +0530

    [NEW] change carusal to react-alice-carousel

--- components/CampaignsCarousal/index.jsx
+++ components/CampaignsCarousal/index.jsx
@@ -1,33 +1,52 @@
 import { Typography } from "@mui/material";
-import { useStyles } from "styles/IndividualIdea/RelatedCampaigns/RelatedCampaigns.style";
-import Carousel from 'react-elastic-carousel'
+import { useStyles } from "styles/RelatedCampaigns/RelatedCampaigns.style";
+// import Carousel from 'react-elastic-carousel'
+
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+
 import DiscoverCard from "components/DiscoverCard";
 
 const CampaignCarousal = ({ color, bgColor, heading }) => {
     const classes = useStyles();
-    const breakPoints = [
-        { width: 1, itemsToShow: 1 },
-        { width: 550, itemsToShow: 3 },
-        { width: 768, itemsToShow: 4 }
-    ]
 
-    const campaigns = [1, 2, 3, 4]
+    const responsive = {
+        428: { items: 1 },
+        768: { items: 2 },
+        1024: { items: 4 },
+    };
+
+    const campaigns = [
+        <DiscoverCard />,
+        <DiscoverCard />,
+        <DiscoverCard />,
+        <DiscoverCard />,
+        <DiscoverCard />,
+    ];
 
     return (
-        <div style={{ backgroundColor: bgColor, color: color }} className={classes.container}>
-            <Typography component={'h1'} className={classes.campaignHeader} variant='heading' >
+        <div
+            style={{ backgroundColor: bgColor, color: color }}
+            className={classes.container}
+        >
+            <Typography
+                style={{
+                    marginBottom: "2rem",
+                }}
+                component={"h1"}
+                variant="heading"
+            >
                 {heading}
             </Typography>
-            <Carousel infinite breakPoints={breakPoints}>
-                {campaigns.map((testimonial) => {
-                    return (
-                        <DiscoverCard />
-                    )
-                })}
-            </Carousel>
 
+            <AliceCarousel
+                mouseTracking
+                items={campaigns}
+                responsive={responsive}
+            // controlsStrategy="alternate"
+            />
         </div>
-    )
-}
+    );
+};
 
-export default CampaignCarousal
+export default CampaignCarousal;
