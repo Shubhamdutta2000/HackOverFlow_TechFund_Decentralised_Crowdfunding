35ac8e622a8ec58dd72af98b09fa5ffabe3f192a
AmitrajitDas <amitrajitdas31@gmail.com>
2022-02-27 16:34:20 +0530

    persist:  theme

--- config/theme.js
+++ config/theme.js
@@ -1,42 +1,42 @@
-import { createTheme } from "@mui/material/styles";
-
-
+import { createTheme } from '@mui/material/styles'
+import { red } from '@mui/material/colors'
 // Create a theme instance.
 const theme = createTheme({
-  // root: {
-  //   fontFamily: "'Circular Std', sans-serif",
-  // },
   palette: {
-    primary: { main: "#333CFF" }, // deep blue
-    // secondary: { main: "#141414" }, // black
-    alternate: { main: "#fff" },
-    error: { main: "#F8485E" }, // red
-    background: {
-      primary: "#fff",
-      secondary: "#141414",
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    button: {
+      main: '#333CFF',
+    },
+    error: {
+      main: red.A400,
     },
   },
   typography: {
     heading: {
-      fontFamily: "'Circular Std', sans-serif",
+      fontFamily: "'Circular Std','Shizuru', sans-serif",
     },
     description: {
-      fontFamily: "'Euclid Circular A', sans-serif",
+      fontFamily: "'Euclid Circular A','Shizuru', sans-serif",
     },
     h2: {
-      fontFamily: "Circular Std",
-      fontStyle: "normal",
-      fontWeight: "500",
-      fontSize: "72px",
-      lineHeight: "107%",
+      fontFamily: 'Circular Std',
+      fontStyle: 'normal',
+      fontWeight: '500',
+      fontSize: '72px',
+      lineHeight: '107%',
     },
     body2: {
-      fontFamily: "Euclid Circular A",
-      fontStyle: "normal",
-      fontWeight: "500",
-      fontSize: "27px",
-      lineHeight: "107%",
-      color: "#252525",
+      fontFamily: 'Euclid Circular A',
+      fontStyle: 'normal',
+      fontWeight: '500',
+      fontSize: '27px',
+      lineHeight: '107%',
+      color: '#252525',
     },
     ideaStats: {
       fontFamily: 'Euclid Circular A',
