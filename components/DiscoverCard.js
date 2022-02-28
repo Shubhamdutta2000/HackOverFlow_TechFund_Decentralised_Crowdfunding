0018570bcd60a9ca95403c262a1fd2deb4a7614a
AmitrajitDas <amitrajitdas31@gmail.com>
2022-02-28 18:39:45 +0530

    new: Discover card dynamic

--- components/DiscoverCard.js
+++ components/DiscoverCard.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-
+import Link from 'next/link'
 import {
   Container,
   Card,
@@ -15,125 +15,127 @@ import CardBluePartBg from '../public/assets/desktop/CardBluePartBg.png'
 
 import { useStyles } from '../styles/DiscoverCard.style.js'
 
-export default function DiscoverCard() {
+export default function DiscoverCard({ idea }) {
   const classes = useStyles()
 
   return (
-    <Card className={classes.discoverCard}>
-      <CardActionArea>
-        <div className={classes.discoverCardImg}>
-          <Image
-            height="1050"
-            className={classes.discoverCardImgCover}
-            src={DiscoverCardIdeaCoverImg}
-            alt='HeroBg'
-            objectFit='cover'
-          />
-        </div>
-        <CardContent style={{ marginTop: '0', paddingTop: '0' }}>
-          <Typography
-            gutterBottom
-            variant='description'
-            component='div'
-            className={classes.discoverCard_badge}
-          >
-            Design
-          </Typography>
-          <Typography
-            gutterBottom
-            variant='description'
-            component='div'
-            className={classes.discoverCard_heading}
-          >
-            Learn Web 3.0
-          </Typography>
-          <Typography
-            variant='description'
-            className={classes.discoverCard_description}
-          >
-            Lizards are a widespread group of squamate reptiles...
-          </Typography>
-          <div className={classes.discoverCard_innovatorDiv}>
-            <Typography
-              variant='description'
-              color='text.secondary'
-              className={classes.discoverCard_innovatorName}
-            >
-              @ Stylercross Pvt. Ltd.
-            </Typography>
-            <img
-              className={classes.discoverCard_innovatorPic}
-              src='https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/d0/d0ca94e64f22a73c804405bd2565d6221ae6f1a2_full.jpg'
-              alt='abcd'
+    <Link href={`ideas/${idea?.objectId}`} passHref={true}>
+      <Card className={classes.discoverCard}>
+        <CardActionArea>
+          <div className={classes.discoverCardImg}>
+            <Image
+              height='1050'
+              className={classes.discoverCardImgCover}
+              src={DiscoverCardIdeaCoverImg}
+              alt='HeroBg'
+              objectFit='cover'
             />
           </div>
-        </CardContent>
+          <CardContent style={{ marginTop: '0', paddingTop: '0' }}>
+            <Typography
+              gutterBottom
+              variant='description'
+              component='div'
+              className={classes.discoverCard_badge}
+            >
+              Design
+            </Typography>
+            <Typography
+              gutterBottom
+              variant='description'
+              component='div'
+              className={classes.discoverCard_heading}
+            >
+              {idea && idea.title}
+            </Typography>
+            <Typography
+              variant='description'
+              className={classes.discoverCard_description}
+            >
+              {idea && idea.description && idea.description.substring(0, 50)}...
+            </Typography>
+            <div className={classes.discoverCard_innovatorDiv}>
+              <Typography
+                variant='description'
+                color='text.secondary'
+                className={classes.discoverCard_innovatorName}
+              >
+                {idea && idea.createdBy}
+              </Typography>
+              <img
+                className={classes.discoverCard_innovatorPic}
+                src='https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/d0/d0ca94e64f22a73c804405bd2565d6221ae6f1a2_full.jpg'
+                alt='abcd'
+              />
+            </div>
+          </CardContent>
 
-        {/* Blue Part */}
-        <CardContent
-          style={{
-            margin: "0",
-            padding: "0",
-            position: "relative",
-            // top: "0.28rem",
-          }}
-        >
-          <div style={{ position: "relative", margin: "0", padding: "0" }}>
-            <Image src={CardBluePartBg} alt="HeroBg" />
-          </div>
-          <div
+          {/* Blue Part */}
+          <CardContent
             style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              position: 'absolute',
-              width: '100%',
-              top: '0',
-              zIndex: '10',
-              padding: '0.8rem 1rem',
+              margin: '0',
+              padding: '0',
+              position: 'relative',
+              // top: "0.28rem",
             }}
           >
-            <div>
-              <Typography
-                gutterBottom
-                variant='description'
-                className={classes.discoverCardBluePart_ethRaised}
-              >
-                2 ETH
-              </Typography>
-              <Typography
-                gutterBottom
-                variant='description'
-                className={classes.discoverCardBluePart_totalEth}
-              >
-                raised of 5 ETH
-              </Typography>
+            <div style={{ position: 'relative', margin: '0', padding: '0' }}>
+              <Image src={CardBluePartBg} alt='HeroBg' />
             </div>
-            <div>
-              <Typography
-                gutterBottom
-                variant='description'
-                className={classes.discoverCardBluePart_ethRaised}
-              >
-                12
-              </Typography>
-              <Typography
-                gutterBottom
-                variant='description'
-                className={classes.discoverCardBluePart_totalEth}
-              >
-                contributors
-              </Typography>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                position: 'absolute',
+                width: '100%',
+                top: '0',
+                zIndex: '10',
+                padding: '0.8rem 1rem',
+              }}
+            >
+              <div>
+                <Typography
+                  gutterBottom
+                  variant='description'
+                  className={classes.discoverCardBluePart_ethRaised}
+                >
+                  2 ETH
+                </Typography>
+                <Typography
+                  gutterBottom
+                  variant='description'
+                  className={classes.discoverCardBluePart_totalEth}
+                >
+                  raised of 5 ETH
+                </Typography>
+              </div>
+              <div>
+                <Typography
+                  gutterBottom
+                  variant='description'
+                  className={classes.discoverCardBluePart_ethRaised}
+                >
+                  12
+                </Typography>
+                <Typography
+                  gutterBottom
+                  variant='description'
+                  className={classes.discoverCardBluePart_totalEth}
+                >
+                  contributors
+                </Typography>
+              </div>
             </div>
-          </div>
-          <Typography
-            variant='description'
-            className={classes.discoverCardBluePart_endingIn}
-          >
-            <span style={{ color: '#FFABAB' }}>Ending in </span>
-            &nbsp; 1hr 30min 45sec
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+            <Typography
+              variant='description'
+              className={classes.discoverCardBluePart_endingIn}
+            >
+              <span style={{ color: '#FFABAB' }}>Ending in </span>
+              &nbsp; 1hr 30min 45sec
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Link>
   )
 }
